1b57df8e341d1b872f10dbdbf95a76d106e7c655
arodas08 <adanrodas08@gmail.com>
2022-05-04 16:13:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://images.vexels.com/media/users/3/70747/raw/7c2cf8ecfa6daba57c044ff043035814-silueta-dj-nino-con-altavoces.png" className="App-logo" alt="logo" />
+        <p>
+          Esto me costo un huevo <code>src/App.js</code> Ovalema.com
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vamos Bitches..!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
