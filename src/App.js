2a371e33ed4baebf570fcb317fec9014285065b0
arodas08 <adanrodas08@gmail.com>
2022-05-04 16:41:56 -0400

    cambios agregamos sitios de twitch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src="https://images.vexels.com/media/users/3/70747/raw/7c2cf8ecfa6daba57c044ff043035814-silueta-dj-nino-con-altavoces.png" className="App-logo" alt="logo" />
         <p>
-          Esto me costo un huevo <code>src/App.js</code> Ovalema.com
+          Hello BTW <code>src/App.js</code> Ovalema.com
         </p>
         <a
           className="App-link"
